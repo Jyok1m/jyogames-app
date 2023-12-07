1975626f09c7e16c7e574a79c4ecba14906c6cda
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-12-07 11:55:32 +0100

    fixed i18n file path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const withNextIntl = require("next-intl/plugin")("./middlewares/next-intl/i18n.js");
+const withNextIntl = require("next-intl/plugin")("./i18n.js");
 
 module.exports = withNextIntl({
 	reactStrictMode: false,
