5ac70bf7e07ea70fde9560e57b83ac68be8968b6
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-12-13 09:03:08 +0100

    configured redirection

--- components/global/Header.jsx
+++ components/global/Header.jsx
@@ -2,12 +2,12 @@
 
 import { Fragment, useState, useEffect } from "react";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname, useParams, useSelectedLayoutSegment } from "next/navigation";
 import socketIOClient from "socket.io-client";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon, GlobeAltIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import { useSelector, useDispatch } from "react-redux";
-import { disconnectUser } from "@/lib/reducers/user";
+import { disconnectUser, changeLocale } from "@/lib/reducers/user";
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(" ");
@@ -17,9 +17,21 @@ export default function Header() {
 	const t = useTranslations("header");
 	const router = useRouter();
 	const dispatch = useDispatch();
-	const { uid } = useSelector((state) => state.user.value);
+
+	/* ---------------------------------------------------------------- */
+	/*                               URLS                               */
+	/* ---------------------------------------------------------------- */
+	const pathname = usePathname();
+	const params = useParams();
+	const slugSegment = useSelectedLayoutSegment() || "";
+	const { uid, locale } = useSelector((state) => state.user.value);
+
+	/* ---------------------------------------------------------------- */
+	/*                            State hooks                           */
+	/* ---------------------------------------------------------------- */
 
 	const [socket, setSocket] = useState(null);
+	const [isMobile, setIsMobile] = useState(false);
 
 	// Handle socket connection with backend
 	useEffect(() => {
@@ -35,11 +47,24 @@ export default function Header() {
 			}
 		});
 
+		window.addEventListener("resize", () => {
+			setIsMobile(window.innerWidth < 640);
+		});
+
 		return () => {
 			socket.off("connect"); // Débranche l'écoute du socket "connect"
 			socket.disconnect(); // Déconnecte le socket du serveur
+
+			window.removeEventListener("resize", () => {
+				setIsMobile(window.innerWidth < 640);
+			});
 		};
 	}, [uid]);
+
+	useEffect(() => {
+		setIsMobile(window.innerWidth < 640);
+		dispatch(changeLocale(params.locale));
+	}, [pathname]);
 
 	const handleDisconnection = async () => {
 		const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/sign-out`, {
@@ -69,10 +94,14 @@ export default function Header() {
 						<div className="relative flex h-16 justify-between">
 							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 								{/* Mobile menu button */}
-								<Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+								<Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-white">
 									<span className="absolute -inset-0.5" />
 									<span className="sr-only">Open main menu</span>
-									{open ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
+									{open ? (
+										<XMarkIcon className="h-8 w-8 font-medium text-white cursor-pointer hover:text-indigo-500" aria-hidden="true" />
+									) : (
+										<Bars3Icon className="h-8 w-8 font-medium text-white cursor-pointer hover:text-indigo-500" aria-hidden="true" />
+									)}
 								</Disclosure.Button>
 							</div>
 							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -108,14 +137,52 @@ export default function Header() {
 								</div>
 							</div>
 							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+								<Menu as="div" className="relative ml-3">
+									<div>
+										<Menu.Button className="rounded-md px-2 py-2 text-sm font-semibold text-white shadow-sm">
+											<GlobeAltIcon className="h-8 w-8 font-medium text-white cursor-pointer hover:text-indigo-500" aria-hidden="true" />
+										</Menu.Button>
+									</div>
+									<Transition
+										as={Fragment}
+										enter="transition ease-out duration-200"
+										enterFrom="transform opacity-0 scale-95"
+										enterTo="transform opacity-100 scale-100"
+										leave="transition ease-in duration-75"
+										leaveFrom="transform opacity-100 scale-100"
+										leaveTo="transform opacity-0 scale-95"
+									>
+										<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+											<Menu.Item>
+												{({ active }) => (
+													<a
+														onClick={() => router.push(`/fr/${slugSegment}`)}
+														className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700 cursor-pointer")}
+													>
+														Français
+													</a>
+												)}
+											</Menu.Item>
+											<Menu.Item>
+												{({ active }) => (
+													<a
+														onClick={() => router.push(`/en/${slugSegment}`)}
+														className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700 cursor-pointer")}
+													>
+														English
+													</a>
+												)}
+											</Menu.Item>
+										</Menu.Items>
+									</Transition>
+								</Menu>
+
 								{/* Profile dropdown */}
 
 								{uid && (
 									<Menu as="div" className="relative ml-3">
 										<div>
 											<Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-												<span className="absolute -inset-1.5" />
-												<span className="sr-only">Open user menu</span>
 												<img
 													className="h-8 w-8 rounded-full"
 													src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
@@ -161,7 +228,18 @@ export default function Header() {
 										</Transition>
 									</Menu>
 								)}
-								{!uid && (
+
+								{!uid && isMobile && (
+									<button
+										type="button"
+										className="h-8 w-8 font-medium text-white cursor-pointer hover:text-indigo-500"
+										onClick={() => router.push("/auth")}
+									>
+										<UserCircleIcon className="h-8 w-8 font-medium text-white cursor-pointer" />
+									</button>
+								)}
+
+								{!uid && !isMobile && (
 									<button
 										type="button"
 										className="rounded-md bg-indigo-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
